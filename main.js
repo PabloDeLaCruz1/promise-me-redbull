c95bf01b14a182bcb8becbcb2d6ee1258fb760c8
Pablo De La Cruz <squall07524@gmail.com>
2018-08-16 03:31:36 -0400

    mvp, axios gets data then dynamic display

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
+let div = document.getElementById("mainContainer");
+
+
 axios.get('https://www.redbullshopus.com/products.json')
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+    .then(function (response) {
+        let res = response.data.products;
+        console.log(res);
+        
+        res.forEach(element => {
+            
+            let divRow = document.createElement("div");
+            divRow.setAttribute("class", "row margin");
+            divRow.innerHTML = `<div class="card">
+            <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator img" src="${element.images[0].src}">
+            </div>
+            <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4">${element.title}
+                    <i class="material-icons right">more_vert</i>
+                </span>
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">${element.title}
+                    <i class="material-icons right">close</i>
+                </span>
+                ${element.body_html} </div>
+        </div>`;
+            div.appendChild(divRow);
+        });
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
